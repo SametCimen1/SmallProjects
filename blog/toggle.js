59805fbbb3136e4571529400153792f87960327f
SametCimen1 <cimensamet338@gmail.com>
2021-06-22 21:23:45 -0400

    updated blog

--- blog/toggle.js
+++ blog/toggle.js
@@ -1,6 +1,9 @@
-const toggle = document.getElementsByClassName("toggle-button")[0];
+const toggle = document.querySelector(".toggle-button");
 const navBarLinks = document.getElementsByClassName("navbar-links")[0];
+const navBar = document.getElementsByClassName("navBar")[0];
+
 
 toggle.addEventListener('click', () =>{
     navBarLinks.classList.toggle('active')
+    navBar.classList.toggle('active')
 })
