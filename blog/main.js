1ba41938b7acd3c9119052d1cff3b0a12888261a
SametCimen1 <cimensamet338@gmail.com>
2021-06-24 18:55:25 -0400

    updated text animations

--- blog/main.js
+++ blog/main.js
@@ -1,22 +1,30 @@
 
-// var links = document.querySelectorAll(".myLink");
-
-
-
-// for(let i = 0; i<links.length; i++){
-//   links[i].addEventListener("mouseout", ()=>{
-//     console.log(links[i].classList.indexOf)
-    
-//     links[i].classList.add("mouseMovedOutside")
-    
-//   })
-//   }
+var links = document.querySelectorAll(".socialMedia");
+var heyText = document.querySelector(".hey");
+heyText.addEventListener("mouseover", ()=>{
+  heyText.classList.add("mouseHoverInText")
+  heyText.classList.remove("mouseHoverOutText")
+})
+heyText.addEventListener("mouseout", ()=>{
+  heyText.classList.remove("mouseHoverInText")
+  heyText.classList.add("mouseHoverOutText")
+})
+for(let i = 0; i<links.length; i++){
+  links[i].addEventListener("mouseout", ()=>{
+    links[i].classList.remove("mouseHoverIn")
+    links[i].classList.add("mouseHoverOut")
+  })
+  links[i].addEventListener("mouseover", ()=>{
+    links[i].classList.remove("mouseHoverOut")
+    links[i].classList.add("mouseHoverIn")
+  })
+  }
    
    
-// const isItInList = elem =>{
-//   for(let i = 0; i <elem.length; i++){
-//     if(elem[i] === "hovored"){}
-//   }
-// }
+const isItInList = elem =>{
+  for(let i = 0; i <elem.length; i++){
+    if(elem[i] === "hovored"){}
+  }
+}
  
  
